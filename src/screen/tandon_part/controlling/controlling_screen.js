10766b8f0e32ab3c01ad60cdd4d7b8d0b7486479
azrinaldi <90147569+azrinaldi@users.noreply.github.com>
2023-11-27 10:33:35 +0700

    Update controlling_screen.js

--- src/screen/tandon_part/controlling/controlling_screen.js
+++ src/screen/tandon_part/controlling/controlling_screen.js
@@ -1,49 +1,88 @@
-import React, { useState, useEffect, useCallback } from "react";
-import {
-    View,
-    Text,
-    TouchableOpacity,
-    StatusBar,
-    SafeAreaView,
-    ImageBackground,
-    ScrollView,
-    RefreshControl
-} from 'react-native';
-import { useDispatch, useSelector } from 'react-redux';
+import React, {useState, useEffect, useCallback} from 'react';
+import {View, ScrollView, RefreshControl} from 'react-native';
+
+import styles from './controlling_style';
+import CardMonitoring from '../../../component/card_monitoring';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useDispatch, useSelector} from 'react-redux';
+import {getAktuatorById} from '../../../redux/action';
+import Loading from '../../../component/loading';
 
-import styles from "./controlling_style";
-import stylesGlobal from "../../../utils/style_global";
-
-import CardMonitoring from "../../../component/card_monitoring";
-import Loading from "../../../component/loading";
-import { getApiAktuatorTandon } from "../../../redux/action";
-
-
-
-const wait = (timeout) => {
-    return new Promise(resolve => {
-        setTimeout(resolve, timeout)
-    });
-}
-console.log("Controlling Screen: ",)
-const ControllingScreenTandon = (props) => {
-    const dispatch = useDispatch();
-    const id = props.data.idData
-
-    useEffect(() => {
-        AsyncStorage.getItem('token').then(token => {
-            dispatch(getApiAktuatorTandon(id, token));
-        });
-    }, [dispatch, id]);
-
-    const aktuatorData = useSelector(state => state.userReducer.dataListAktuatorTandon);
-    console.log("inni akturaoriaois:")
-    return (
-        <View>
-
-        </View>
-    )
+const wait = timeout => {
+  return new Promise(resolve => {
+    setTimeout(resolve, timeout);
+  });
 };
 
-export default ControllingScreenTandon
+const ControllingScreenTandon = props => {
+  const [refreshing, setRefreshing] = useState(false);
+  const [isLoading, setLoading] = useState(true);
+
+  const id = props.data.idData;
+
+  const onRefresh = useCallback(() => {
+    setLoading(true);
+    setRefreshing(true);
+    AsyncStorage.getItem('token').then(respons => {
+      dispatch(getAktuatorTandonById(id, respons));
+    });
+    wait(3000).then(() => {
+      setRefreshing(false);
+      setLoading(false);
+    });
+  }, []);
+
+  const dispatch = useDispatch();
+
+  const {dataAktuatorTandonById, menuMoCon} = useSelector(
+    state => state.userReducer,
+  );
+
+  const getApiById = () => {
+    AsyncStorage.getItem('token').then(respons => {
+      dispatch(getAktuatorTandonById(id, respons));
+      setLoading(false);
+    });
+  };
+
+  useEffect(() => {
+    getApiById();
+  }, [menuMoCon]);
+
+  console.log("Data Aktuator Tandon:    ",dataAktuatorTandonById)
+
+  return (
+    <>
+      {dataAktuatorTandonById != undefined && isLoading == false ? (
+        <View style={styles.container}>
+          <ScrollView
+            refreshControl={
+              <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+            }>
+            {dataAktuatorTandonById != undefined
+              ? dataAktuatorTandonById.map(value => {
+                  return (
+                    // <CardMonitoring
+                    //   data={{
+                    //     id: value.id,
+                    //     icon: value.icon,
+                    //     name: value.name,
+                    //     color: value.color,
+                    //     topic_broker: value.topic_broker,
+                    //     updated_at: value.updated_at,
+                    //     status_lifecycle: value.status_lifecycle,
+                    //   }}
+                    // />
+                  );
+                })
+              : null}
+          </ScrollView>
+        </View>
+      ) : (
+        <Loading />
+      )}
+    </>
+  );
+};
+
+export default ControllingScreenTandon;
